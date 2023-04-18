e8d95737203398ba355c94918b63aa4d2a522d98
MyNameIsAlpaca <Saragosagabriele@gmail.com>
2023-04-18 15:24:55 +0200

    feat: add style with bootstrap

--- index.php
+++ index.php
@@ -49,17 +49,38 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Hotel BoVaGo</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 <body>
-  <div class="hotel-container">
-    <?php 
-    foreach($hotels as $hotel)
-
-      foreach($hotel as $hotelInfo)
-
-      echo $hotelInfo . "<br>"
-
-    ?>
-  </div>
+    <div class="hotel">
+        <table class="table table-striped table-dark">
+            <thead>
+                <tr>
+                <th scope="col">Nome</th>
+                <th scope="col">Descrizione</th>
+                <th scope="col">Parcheggio</th>
+                <th scope="col">Voto</th>
+                <th scope="col">Distanza dal centro</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php 
+            foreach($hotels as $hotel){
+                ?>
+                    <tr>
+                    <th scope="row"><?php echo $hotel['name'] ?></th>
+                    <td><?php echo $hotel['description'] ?></td>
+                    <td><?php echo $hotel['parking'] ? 'Si' : 'No' ?></td>
+                    <td><?php echo $hotel['vote'] ?></td>
+                    <td><?php echo $hotel['distance_to_center'] ?> Km</td>
+                    </tr>
+                </tbody>
+                <?php
+            }
+            ?>
+        </table>    
+    </div>
+  <!-- Bootstrap -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 </html>
